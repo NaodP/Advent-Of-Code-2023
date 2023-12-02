3befd9e0297fe0578f99b0cb54883341b016c0bc
NaodP <nalf4459@gmail.com>
2023-12-02 13:36:43 -0600

    Day 01 Part 2

--- Day 01/main.py
+++ Day 01/main.py
@@ -1,21 +1,21 @@
 # Naod Philemon
 # 12/01/2023
 
-def findFirst(val):
-	for let in val:
-		if let.isdigit(): 
-			return let
-		
-	return
-
-def findLast(val):
-	for i in range(len(val)-1, -1, -1):
-		if val[i].isdigit(): 
-			return val[i]
-		
-	return
-
 def part1(values):
+	def findFirst(val):
+		for let in val:
+			if let.isdigit(): 
+				return let
+			
+		return
+
+	def findLast(val):
+		for i in range(len(val)-1, -1, -1):
+			if val[i].isdigit(): 
+				return val[i]
+			
+		return
+
 	sum = 0
 
 	for val in values:
@@ -23,8 +23,58 @@ def part1(values):
 
 	return sum
 
-def part2():
-	pass
+def part2(values):
+	def findFirst(value):
+		tempValue = ""
+		found = False
+		val = 0
+
+		for let in value:
+			if let.isdigit(): 
+				return let
+			
+			tempValue += let
+
+			for index, num in enumerate(word_nums):
+				if num in tempValue:
+					val = index + 1
+					found = True
+					break
+			
+			if found: 
+				break
+		
+		return str(val)
+
+	def findLast(value):
+		tempValue = ""
+		found = False
+		val = 0
+
+		for i in range(len(value)-1, -1, -1):
+			if value[i].isdigit(): 
+				return value[i]
+			
+			tempValue = value[i] + tempValue
+
+			for index, num in enumerate(word_nums):
+				if num in tempValue:
+					val = index + 1
+					found = True
+					break
+			
+			if found: 
+				break
+		
+		return str(val)
+
+	word_nums = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+	result = 0
+
+	for value in values:
+		result += int(findFirst(value) + findLast(value))
+
+	return result
 
 def main():
 	file = open('./input.txt')
@@ -34,6 +84,6 @@ def main():
 		values.append(line)
 
 	print('Part 1 -', part1(values))
-	print('Part 2 -', part2())
+	print('Part 2 -', part2(values))
 
 main()
